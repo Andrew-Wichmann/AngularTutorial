5dfe8cfd35884a7760c960dafdaf2903e8e441ca
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-02-20 15:20:42 -0600

    Finalized the Angular structure branch.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,2 @@
-app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
-  forecast.success(function(data) {
-    $scope.fiveDay = data;
-  });
+app.controller('MainController');
 }]);
